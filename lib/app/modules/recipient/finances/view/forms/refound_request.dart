451356e17bd5eeafa61eea2f057e3fcc03d5879d
Italo R. Dev <italorodridev@gmail.com>
2026-02-21 22:46:05 -0300

    ajuste de bugs

--- lib/app/modules/recipient/finances/view/forms/refound_request.dart
+++ lib/app/modules/recipient/finances/view/forms/refound_request.dart
@@ -4,10 +4,13 @@ import 'package:app_colabora_unimedjp/app/modules/recipient/finances/view/forms/
 import 'package:app_colabora_unimedjp/app/modules/recipient/finances/view/forms/assistance/step_4.dart';
 import 'package:app_colabora_unimedjp/app/modules/recipient/finances/view/forms/assistance/step_5.dart';
 import 'package:app_colabora_unimedjp/app/modules/recipient/finances/view/forms/send_solicitation_modal.dart';
+import 'package:app_colabora_unimedjp/app/services/config.service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:liquid_swipe/PageHelpers/LiquidController.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
+import '../../../../utils/components/progress_app.component.dart';
 import '../../../../utils/components/toolbars/toolbar_app.component.dart';
 import '../../controller/financial.controller.dart';
 import '../components/form_swipe.dart';
@@ -16,6 +19,7 @@ enum RefundType { financeiro, assistencial }
 
 class RefundRequestPage extends StatelessWidget {
   final FinancialController ctrl = Get.put(FinancialController());
+  ConfigService configService = Get.find<ConfigService>();
   final RefundType type;
   final LiquidController _controller = LiquidController();
 
@@ -23,32 +27,37 @@ class RefundRequestPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        bottom: PreferredSize(
-          preferredSize: Size(double.infinity, 0),
-          child: ToolbarAppComponent(
-            onPressedMenu: () {},
-            showMenu: false,
-            title: type == RefundType.assistencial
-                ? 'Reembolso assistencial'
-                : 'Reembolso financeiro',
+    return LoadingOverlay(
+      color: Colors.black.withAlpha(40),
+      isLoading: configService.isLoading,
+      progressIndicator: ProgressAppComponent(),
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: PreferredSize(
+            preferredSize: Size(double.infinity, 0),
+            child: ToolbarAppComponent(
+              onPressedMenu: () {},
+              showMenu: false,
+              title: type == RefundType.assistencial
+                  ? 'Reembolso assistencial'
+                  : 'Reembolso financeiro',
+            ),
           ),
         ),
-      ),
-      body: SmartLiquidForm(
-        liquidSwipeController: _controller,
-        labelButtonFinish: 'Enviar solicitação',
-        steps: [
-          Step1FormAssistence(),
-          Step2FormAssistence(),
-          Step3FormAssistence(),
-          Step4FormAssistence(),
-          Step5FormAssistence(),
-        ],
-        onFinish: () {
-          ctrl.sendSolucitation();
-        },
+        body: SmartLiquidForm(
+          liquidSwipeController: _controller,
+          labelButtonFinish: 'Enviar solicitação',
+          steps: [
+            Step1FormAssistence(),
+            Step2FormAssistence(),
+            Step3FormAssistence(),
+            Step4FormAssistence(),
+            Step5FormAssistence(),
+          ],
+          onFinish: () {
+            ctrl.sendSolucitation();
+          },
+        ),
       ),
     );
   }
